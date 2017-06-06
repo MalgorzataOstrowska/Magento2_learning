2fa0e4de531db19992b94e68823660d52b86b4da
Malgorzata <ostrowska.malgorzata.ewa@gmail.com>
2017-06-06 14:49:43 +0200

    Add controller

---
+++ app/code/Cminds/Helloworld/Controller/Index/Index.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cminds\Helloworld\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+
+{
+    protected $_resultPageFactory;
+
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        $this->_resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+        return $resultPage;
+    }
+}
