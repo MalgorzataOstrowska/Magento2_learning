e3ec30df58be503e7e34668becd44d80a26b4993
Malgorzata <ostrowska.malgorzata.ewa@gmail.com>
2017-06-09 12:00:45 +0200

    Fix frontend layout, template, block and controller

--- app/code/Cminds/Helloworld/Block/Helloworld.php
+++ app/code/Cminds/Helloworld/Block/Helloworld.php
@@ -2,9 +2,7 @@
 namespace Cminds\Helloworld\Block;
 
 use Cminds\Helloworld\Helper\Data;
-use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Template;
-use Magento\Store\Model\ScopeInterface;
 
 class Helloworld extends Template
 {
@@ -14,34 +12,19 @@ class Helloworld extends Template
     protected $helper;
 
     /**
-     * @var ScopeConfigInterface
-     */
-    protected $scopeConfig;
-
-    protected $objectManager;
-
-
-    /**
      * Helloworld constructor.
      * @param Template\Context $context
      * @param array $data
      * @param Data $dataHelper
-//     * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
         Template\Context $context,
         array $data = [],
         Data $dataHelper
-//        ScopeConfigInterface $scopeConfig     // Errors during compilation:
-                                                // Incorrect dependency in class (...) ScopeConfigInterface already exists in context object
-
-                                                // this class is already part of the parent
     )
     {
-        $this->helper = $dataHelper;
-//        $this->scopeConfig = $scopeConfig;
-
         parent::__construct($context, $data);
+        $this->helper = $dataHelper;
     }
 
     public function getHelloWorldText()
@@ -49,11 +32,6 @@ class Helloworld extends Template
         $color =  $this->helper->getColor();
 
         return 'Hello ' . $color . ' world!';
-    }
-
-    public function getTextFromConfiguration()
-    {
-        return 'From block: ' . $this->scopeConfig->getValue('helloworld/general/display_text', ScopeInterface::SCOPE_STORE);
     }
 
     public function getTextFromConfigurationFromHelperThroughBlock(){
--- app/code/Cminds/Helloworld/Controller/Index/Index.php
+++ app/code/Cminds/Helloworld/Controller/Index/Index.php
@@ -4,7 +4,6 @@ namespace Cminds\Helloworld\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\DataObject;
 use Magento\Framework\View\Result\PageFactory;
 
 class Index extends Action
@@ -20,11 +19,6 @@ class Index extends Action
 
     public function execute()
     {
-        $textDisplay = new DataObject(array('text' => 'Mageplaza'));
-        $this->_eventManager->dispatch('cminds_helloworld_display_text', ['text' => $textDisplay]);
-        echo $textDisplay->getText();
-//        exit;
-
         $resultPage = $this->_resultPageFactory->create();
 
         return $resultPage;
